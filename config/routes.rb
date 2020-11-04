09774cf406b6e02d5047aa9430dbacfc84e0c833
travis_gandy <travis_gandy@yahoo.com>
2020-11-03 21:26:03 -0500

    band view form partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :songs
   resources :bands
+
+  root 'bands#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
