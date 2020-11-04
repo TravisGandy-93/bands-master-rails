f8338e9064e2be1c80e7aaee75a0b0ac5eaa7485
travis_gandy <travis_gandy@yahoo.com>
2020-11-04 11:27:01 -0500

    more devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :bands
     has_many :songs, through: :bands
-    devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
     validates :username, uniqueness: true, presence: true
     has_secure_password
 end
